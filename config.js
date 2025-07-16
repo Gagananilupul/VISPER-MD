43288e4a18e6ed85818849fd12225281159d64ec
Gagananilupul <gagananilupul2749@gmail.com>
2025-07-16 18:27:33 +0530

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ module.exports = {
 
 SESSION_ID: process.env.SESSION_ID === undefined ? 'VISPER-MD&Th4w0QLI#D0sAMROangyUD2PdbRK7Jt2T6VebOSN6P8V8_rxgsu0' : process.env.SESSION_ID, 
 PORT: process.env.PORT === undefined ? '8000' : process.env.PORT,    
-SESSION_NAME: process.env.SESSION_NAME === undefined ? 'Nilupul X' : process.env.SESSION_NAME, 
+SESSION_NAME: process.env.SESSION_NAME === undefined ? 'Safari(mac os)' : process.env.SESSION_NAME, 
 
 };
 
